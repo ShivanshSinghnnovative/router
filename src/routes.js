47dc55b0f2f7f478fc6447908ea8160c1c1be257
ShivanshSinghnnovative <shivansh.singh@innovative.in>
2023-09-21 17:02:30 +0530

    codess

---
+++ src/routes.js
@@ -0,0 +1,39 @@
+import {createWebHistory,createRouter} from 'vue-router'
+const HomePage = () => import('./views/HomePage.vue')
+const AboutPage = () => import('./views/AboutPage.vue')
+const JobPage =() => import('./views/JobPage.vue')
+const JobDetails=() => import('./components/JobDetails.vue')
+const NotFound=() => import( './components/PageNotfound.vue')
+const routes=[
+    {
+        name:'HomePage',
+        path:'/',
+        component:HomePage
+    },
+    {
+        name:'AboutPage',
+        path:'/about',
+        component:AboutPage
+    },
+    {
+        name:'JobPage',
+        path:'/jobs',
+        component:JobPage
+    },
+    {
+        name:'JobDetails',
+        path:'/jobs/:id',
+        component:JobDetails
+    },
+    {
+        name:'NotFound',
+        path:'/:pathMatch(.*)',
+        component:NotFound
+    }
+  
+]
+const router = createRouter({
+    history:createWebHistory(),
+    routes
+});
+export default router;
